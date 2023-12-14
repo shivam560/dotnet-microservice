226cd373b19fb110c4fd920b6dfa9956fecd60eb
shivam <shivamdldav@gmail.com>
2023-12-14 20:35:14 +0530

    Initial code commit added

---
+++ Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Services.CouponAPI;
+
+public class Coupon
+{
+    [Key]
+    public int CouponId {get;set;}
+    [Required]
+    public string CouponCode {get;set;}
+    [Required]
+    public double DiscountAmount {get;set;}
+    public int MinAmount {get;set;}
+    
+
+}
